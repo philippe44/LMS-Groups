fbabd8cca74f68d6bf164d8140d67b893b25b400
Philippe G <philippe44@users.noreply.github.com>
2021-01-13 23:21:27 -0800

    Revert "0.14.0"
    This reverts commit ca3c415229ee185e639daa4d0be4044600e876ec.

---
+++ plugin/Playlist.pm
@@ -0,0 +1,28 @@
+package Slim::Player::Playlist;
+
+=comment
+	This file is overriding the default Slim::Player::Source::stopandClear() method
+	to cover for the Groups plugin's need to have a reference to the original
+	$client object when stopping playback.
+=cut
+
+sub stopAndClear {
+	my $client = shift;
+	
+	# Bug 11447 - Have to stop player and clear song queue
+	# BEGIN - added $client
+	$client->controller->stop($client);
+	# END	
+	$client->controller()->resetSongqueue();
+
+	@{playList($client)} = ();
+	$client->currentPlaylist(undef);
+	
+	# Remove saved playlist if available
+	my $playlistUrl = _playlistUrlForClient($client);
+	unlink(Slim::Utils::Misc::pathFromFileURL($playlistUrl)) if $playlistUrl;
+
+	reshuffle($client);
+}
+
+1;
